0d42b9416f80c5880edad78359ffe37b1ff1b2b5
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2025-08-14 22:55:11 +0900

    NewFileDialog 実装

---
+++ ThGameMgr.Ex.FileManager/NewFileDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace ThGameMgr.Ex.FileManager
+{
+    /// <summary>
+    /// NewFileDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class NewFileDialog : Window
+    {
+        public NewFileDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string? NewFileName { get; set; }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (NewFileNameBox.Text.Length > 0)
+            {
+                this.NewFileName = NewFileNameBox.Text;
+                this.DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show(this, "ファイル名を入力してください。", "新規ファイルの追加",
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+    }
+}
